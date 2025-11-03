0a44f6961675eac81cedff2473e32f0be71670c9
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-11-03 14:00:26 +0600

    create 1st user

--- src/app/module/user/user.router.ts
+++ src/app/module/user/user.router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { UserController } from "./user.contoller";
 
 const router = Router();
 
+router.post("/createuser", UserController.createUser)
 
 export const UserRouter=router
