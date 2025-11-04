73338e39538440da8723ec5160dd955d1769877c
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-11-04 19:38:28 +0600

    user register done with cloudinary setup done

--- src/app/config/envVars.ts
+++ src/app/config/envVars.ts
@@ -1,25 +1,54 @@
 import dotenv from "dotenv";
 
 dotenv.config();
+
+interface CloudinaryConfig {
+  API_KEY: string;
+  API_SECRET: string;
+  CLOUDINARY_URL: string;
+  CLOUDINARY_NAME: string;
+}
+
 interface EnvConfig {
   PORT: string;
-  JWT_SECRET: string;  
-  FRONT_END_URL: string;  
+  JWT_SECRET: string;
+  FRONT_END_URL: string;
+  NODE_ENV: string;
+  SALTROUND: string;
+  CLOUDINARY: CloudinaryConfig;
 }
 
 const loadEnvVariables = (): EnvConfig => {
-  const requierdEnvVariables: string[] = ["PORT", 
+  const requiredEnvVariables: string[] = [
+    "PORT",
     "FRONT_END_URL",
-    "JWT_SECRET"];
-  requierdEnvVariables.forEach((element) => {
-    if (!process.env[element]) {
-      throw new Error(`MIssing require enviroment ${element}`);
+    "JWT_SECRET",
+    "NODE_ENV",
+    "SALTROUND",
+    "API_KEY",
+    "API_SECRET",
+    "CLOUDINARY_URL",
+    "CLOUDINARY_NAME",
+  ];
+
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing required environment variable: ${key}`);
     }
   });
+
   return {
-    PORT: process.env.PORT as string,
-    JWT_SECRET: process.env.JWT_SECRET as string,                        
-    FRONT_END_URL: process.env.FRONT_END_URL as string,                        
+    PORT: process.env.PORT!,
+    JWT_SECRET: process.env.JWT_SECRET!,
+    FRONT_END_URL: process.env.FRONT_END_URL!,
+    NODE_ENV: process.env.NODE_ENV!,
+    SALTROUND: process.env.SALTROUND!,
+    CLOUDINARY: {
+      API_KEY: process.env.API_KEY!,
+      API_SECRET: process.env.API_SECRET!,
+      CLOUDINARY_URL: process.env.CLOUDINARY_URL!,
+      CLOUDINARY_NAME: process.env.CLOUDINARY_NAME!,
+    },
   };
 };
 
--- src/app/module/user/user.router.ts
+++ src/app/module/user/user.router.ts
@@ -1,8 +1,27 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserController } from "./user.contoller";
+import { filUploder } from "../../helpers/fileUploder";
+import validateRequest from "../../middlewares/validateRequest";
+import { UserValidationSchema } from "./user.validation";
 
 const router = Router();
 
-router.post("/createuser", UserController.createUser)
+router.post(
+  "/createuser",
+  filUploder.upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (req.body.data && typeof req.body.data === "string") {
+        req.body = JSON.parse(req.body.data);
+      }
+      UserValidationSchema.createPatientValidationSchema.parse(req.body);
 
-export const UserRouter=router
+      next(); 
+    } catch (error) {
+      return next(error); 
+    }
+  },
+  UserController.createUser
+);
+
+export const UserRouter = router;
