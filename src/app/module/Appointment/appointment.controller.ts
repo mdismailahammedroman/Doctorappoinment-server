a33e8cc059b1e4527ea3a0f7aebcecb59f04b92e
ismailahammed1 <ismailahmmedroman@gmail.com>
2025-11-17 18:06:56 +0600

    Appointment create

---
+++ src/app/module/Appointment/appointment.controller.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import catchAsync from "../../utils/catchAsync";
+import { AppionmentService } from "./appointment.service";
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import { IJWTPayload } from "../../../types/common";
+
+const createAppoinment = catchAsync(
+  async (
+    req: Request & { user?: IJWTPayload },
+    res: Response,
+    next: NextFunction
+  ) => {
+    const user = req.user;
+    const result = await AppionmentService.createAppoinment(
+      user as IJWTPayload,
+      req.body
+    );
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Appoinment create successfuly",
+      data: result,
+    });
+  }
+);
+export const AppoinmentController = {
+  createAppoinment,
+};
